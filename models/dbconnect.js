9d04d18808842c98824b97b46f9ed96d06ca7fa8
bhupendrakhatri2002 <bhupinilgar717@gmail.com>
2024-09-11 16:33:55 +0530

    add mongodb

--- models/dbconnect.js
+++ models/dbconnect.js
@@ -1,5 +1,5 @@
 const mongoose =require("mongoose");
 
-mongoose.connect("mongodb://0.0.0.0/bookstore")
+mongoose.connect("mongodb+srv://bhupinilgar717:bhupendra123@bhupendra.xgc9xwv.mongodb.net/bookstore")
 .then(()=>console.log("data base connected"))
 .catch((err)=>console.log(err))
